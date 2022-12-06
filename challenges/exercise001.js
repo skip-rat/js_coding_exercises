dbbaf7d4dd4df8190f27babcd5da2c9ecc32753e
skip-rat <skip-rat@users.noreply.github.com>
2022-12-06 10:53:10 +0000

    Update exercise001.js
    More elegant solution for ReverseAllWords(). Commented Capitalize(). Formatted with Prettier

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -2,40 +2,32 @@
 
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  
-  if(/[A-Z\W]/.test(word.charAt(0)))
-  {
-    return word;
-  }
-  else
-  {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }
+
+  // if the first letter is a capital return the word
+  if (/[A-Z\W]/.test(word.charAt(0))) return word;
+
+  // Capitilise the first letter of word and join the rest of the letters
+  return word.charAt(0).toUpperCase() + word.slice(1);
+
 }
 
 export function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  
 
   //Frederic", "Bonneville")).toBe("F.B");
-  return firstName.charAt(0) + "." +  lastName.charAt(0);
-
+  return firstName.charAt(0) + "." + lastName.charAt(0);
 }
 
 export function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  
 
   const totalPrice = originalPrice * ((100 + vatRate) / 100);
-  
-  if(Number.isInteger(totalPrice))
-  {
+
+  if (Number.isInteger(totalPrice)) {
     return totalPrice;
-  }
-  else
-  {
+  } else {
     return parseFloat(totalPrice.toFixed(2));
   }
 }
@@ -43,56 +35,48 @@ export function addVAT(originalPrice, vatRate) {
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  
+
   const decreaseValue = (originalPrice * reduction) / 100;
   return originalPrice - decreaseValue.toFixed(2); // implicit cast to number
 }
 
 export function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  
+
   let midPoint;
   let len;
 
-  if(str.length % 2) // even string length
-  {
+  if (str.length % 2) {
+    // even string length
     midPoint = str.length / 2;
     len = 1;
-  }
-  else // odd string length
-  {
+  } // odd string length
+  else {
     midPoint = str.length / 2 - 1;
     len = 2;
   }
-    return str.substring(midPoint, midPoint + len)
+  return str.substring(midPoint, midPoint + len);
 }
 
 export function reverseWord(word) {
-  if (word === undefined) throw new Error("word is required");  
+  if (word === undefined) throw new Error("word is required");
 
-  return word.split('').reverse().join('');
+  return word.split("").reverse().join("");
 }
 
 export function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  
-  const reversedWords = [];
 
-  words.forEach(word => {
-    reversedWords.push(reverseWord(word));    
-  });
-
-  return reversedWords;
+  return words.map((word) => reverseWord(word));
 }
 
 export function countLinuxUsers(users) {
-  if (users === undefined) throw new Error("users is required");  
+  if (users === undefined) throw new Error("users is required");
 
   let count = 0;
 
-  users.forEach(user => {
-    if(user.type === "Linux")
-    {
+  users.forEach((user) => {
+    if (user.type === "Linux") {
       count++;
     }
   });
@@ -106,37 +90,27 @@ export function getMeanScore(scores) {
   let mean = 0;
   let total = 0;
 
-  scores.forEach(num => total += num);
+  scores.forEach((num) => (total += num));
 
   mean = total / scores.length;
 
-  if(Number.isInteger(mean))
-  {
+  if (Number.isInteger(mean)) {
     return mean;
-  }
-  else
-  {
+  } else {
     return parseFloat(mean.toFixed(2));
-  }  
+  }
 }
 
 export function simpleFizzBuzz(n) {
-  if (n === undefined) throw new Error("n is required");  
+  if (n === undefined) throw new Error("n is required");
 
-  if (n % 15 === 0)
-  { 
+  if (n % 15 === 0) {
     return "fizzbuzz";
-  }
-  else if (n % 3 === 0)
-  { 
+  } else if (n % 3 === 0) {
     return "fizz";
-  }
-  else if (n % 5 === 0)
-  { 
+  } else if (n % 5 === 0) {
     return "buzz";
-  }
-  else  
-  {
+  } else {
     return n;
   }
 }
